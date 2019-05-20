22be597c6998ddabbb71720444f7c6faffb66524
kazulive <kazulive0904@gmail.com>
2019-05-20 18:39:30 +0900

    make SRLLIE

---
+++ main_program/estimate_l.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+from padding import psf2otf as psf
+from padding import getKernel as gK
+import glob
+from natsort import natsorted
+
+class IlluminationEstimation(object):
+    def __init__(self, img, alpha):
+        self.alpha = alpha
+        self.height = img.shape[0]
+        self.width = img.shape[1]
+
+        self.kernel = np.array([[-1, 0, 1],
+                                [-2, 0, 2],
+                                [-1, 0, 1]])
+
+        self.F_conj_h = psf(np.expand_dims(np.array([1, -1]), axis=1), (self.height, self.width)).conjugate()
+        self.F_conj_v = psf(np.expand_dims(np.array([1, -1]), axis=1).T, (self.height, self.width)).conjugate()
+        self.F_div = gK(img)[1]
+
+    def get_illumination(self, img, Th, Tv, Zh, Zv, meu):
+        tmp1 = np.fft.fft2(2.0 * img) + meu * self.F_conj_h * np.fft.fft2(Th - Zh/meu) + self.F_conj_v * np.fft.fft2(Tv - Zv/meu)
+        tmp2 = 2.0 + meu * self.F_div
+        return np.real(np.fft.ifft2(tmp1 / tmp2))
+
+    def get_T(self, grad_h, grad_v, Zh, Zv, meu):
+        tmp_h = grad_h + Zh / meu
+        tmp_v = grad_v + Zv / meu
+        return np.sign(tmp_h) * np.maximum(np.abs(tmp_h)-self.alpha/meu, 0.), np.sign(tmp_v) * np.maximum(np.abs(tmp_v)-self.alpha/meu, 0.)
+
+    def get_Z(self, grad_h, grad_v, Th, Tv, Zh, Zv, meu):
+        return Zh + meu * (grad_h - Th), Zv + meu * (grad_v - Tv)
+
+    def main(self, img):
+        illumination = np.copy(img)
+        Th = np.zeros((self.height, self.width), dtype=np.float32)
+        Tv = np.zeros((self.height, self.width), dtype=np.float32)
+        Zh = np.zeros((self.height, self.width), dtype=np.float32)
+        Zv = np.zeros((self.height, self.width), dtype=np.float32)
+        meu = 1.0
+        p = 1.5
+        k = 0
+        while (k < 10):
+            illumination = self.get_illumination(img, Th, Tv, Zh, Zv, meu)
+            cv2.imshow("Illumination", illumination.astype(dtype=np.uint8))
+            cv2.waitKey(0)
+            #illumination = np.clip(illumination, 0, 255)
+            #illumination = np.fix(illumination).astype(dtype=np.uint8)
+            grad_h = cv2.Sobel(img/255.0, cv2.CV_64F, 1, 0, ksize=3)
+            grad_v = cv2.Sobel(img/255.0, cv2.CV_64F, 0, 1, ksize=3)
+            #cv2.imshow("grad_h", grad_h)
+            #cv2.imshow("grad_v", grad_v)
+            #cv2.waitKey(0)
+            Th, Tv = self.get_T(grad_h, grad_v, Zh, Zv, meu)
+            Zh, Zv = self.get_Z(grad_h, grad_v, Th, Tv, Zh, Zv, meu)
+            meu *= p
+            k += 1
+        return illumination
+
+def fileRead():
+    data = []
+    for file in natsorted(glob.glob('./testdata/BMP/*.bmp')):
+        data.append(cv2.imread(file, 1))
+    return data
+
+if __name__ == '__main__':
+    img_list = fileRead()
+    count = 0
+    for img in img_list:
+        count += 1
+        print('input ' + str(count) + ' image')
+        # HSV変換
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+        illumination = IlluminationEstimation(v.astype(dtype=np.float32), 0.007).main(v.astype(dtype=np.float32))
+        #illumination = illumination * 255.0
+        #illumination = np.clip(illumination, 0, 255)
+        #illumination = np.fix(illumination).astype(dtype=np.uint8)
+        cv2.imshow("Illumination", illumination.astype(dtype=np.uint8))#.astype(dtype=np.uint8))
+        cv2.waitKey(0)
---
+++ main_program/padding.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def zero_pad(image, shape, position='corner'):
+    shape = np.asarray(shape, dtype=int)
+    imshape = np.asarray(image.shape, dtype=int)
+    if np.alltrue(imshape == shape):
+        return image
+
+    if np.any(shape <= 0):
+        raise ValueError("ZERO_PAD: null or negative shape given")
+
+    dshape = shape - imshape
+    if np.any(dshape < 0):
+        raise ValueError("ZERO_PAD: target size smaller than source one")
+
+    pad_img = np.zeros(shape, dtype=image.dtype)
+
+    idx, idy = np.indices(imshape)
+
+    if position == 'center':
+        if np.any(dshape % 2 != 0):
+            raise ValueError("ZERO_PAD: source and target shapes "
+                             "have different parity.")
+        offx, offy = dshape // 2
+    else:
+        offx, offy = (0, 0)
+
+    pad_img[idx + offx, idy + offy] = image
+
+    return pad_img
+
+def psf2otf(psf, shape):
+    if np.all(psf == 0):
+        return np.zeros_like(psf)
+
+    inshape = psf.shape
+
+    psf_pad = zero_pad(psf, shape, position='corner')
+
+    for axis, axis_size in enumerate(inshape):
+        psf_pad = np.roll(psf_pad, -int(axis_size / 2), axis=axis)
+
+    otf = np.fft.fft2(psf_pad)
+    n_ops = np.sum(psf_pad.size * np.log2(psf_pad.shape))
+    otf = np.real_if_close(otf, tol=n_ops)
+    return otf
+
+def getKernel(img):#, gamma):
+    sizeF = np.shape(img)
+    kernel = np.expand_dims(np.array([1.0]), axis=1)
+    eigsK = psf2otf(kernel, sizeF)
+    diff_kernelX = np.expand_dims(np.array([1, -1]), axis=1)
+    eigsDtD = np.abs(psf2otf(diff_kernelX, sizeF)) ** 2 + np.abs(psf2otf(diff_kernelX.T, sizeF)) ** 2
+    return eigsK, eigsDtD
